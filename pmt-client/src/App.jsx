5bbb7a95ba40f6372331455395d46ee46753e419
Abinithishkumarprabakaran <abinithishkumarp.a.k@gmail.com>
2023-05-26 21:31:39 +0530

    userauth

--- pmt-client/src/App.jsx
+++ pmt-client/src/App.jsx
@@ -1,17 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
+import { Confirmation } from './components/Login/Confirmation';
+import { ForgotPassword } from './components/Login/ForgotPassword';
+import { Login } from './components/Login/Login'
+import { PageNotFound } from './components/PageNotFound/PageNotFound';
+import { Profile } from './components/Profile/Profile';
+import { ChangePassword } from './components/Signup/ChangePassword';
+import { Signup } from './components/Signup/Signup'
+import './App.css';
+
+import { Routes, Route, Link, useNavigate, Navigate } from "react-router-dom";
 
 function App() {
 
+  const bgStyles = {
+    borderRadius: "0px",
+    backgroundColor: "rgb(141, 45, 232)",
+    minHeight: "100vh",
+  }
+
   return (
-    <>
-      <div>
-        Hello
-      </div>
-    </>
+    <div className="App">
+
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/signup" element={ <Signup /> } />
+        <Route path="/forgotpassword" element={ <ForgotPassword /> } />
+        <Route path="/confirmation" element={ <Confirmation /> } />
+        <Route path="/changepassword/:id" element={ 
+          <ProtectedRoutePasswordChange>
+            <ChangePassword /> 
+          </ProtectedRoutePasswordChange>} />
+        <Route path="/profile" element={ 
+            <ProtectedRouteProfile>
+              <Profile />
+            </ProtectedRouteProfile> }/>
+        <Route path="*" element={ <PageNotFound /> } />
+      </Routes>
+      
+    </div>
+  )
+}
+
+function ProtectedRouteProfile({ children }) {
+  const token = localStorage.getItem('token');
+  return (
+    token ?
+    <section>
+      {children}
+    </section> :
+    <Navigate replace to="/"/>
+  )
+}
+
+function ProtectedRoutePasswordChange({ children }) {
+  const OTP = localStorage.getItem('OTP');
+  return (
+    OTP ?
+    <section>
+      {children}
+    </section> :
+    <Navigate replace to="/"/>
   )
 }
 
 export default App
+
+
---
+++ pmt-client/src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Login } from './Login/Login';
+export { default as ForgotPassword } from './Login/ForgotPassword';
+export { default as Signup } from './Signup/Signup';
+export { default as Confirmation } from './Login/Confirmation';
+export { default as ChangePassword } from './Login/ChangePassword';
+export { default as PageNotFound } from './PageNotFound/PageNotFound';
+export { default as Profile } from './Profile/Profile';
